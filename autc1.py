29d228c51929762c31f7c25a1411b18d12557ef2
Hiori kino <Kino.hiori@nims.go.jp>
2016-10-20 14:06:39 +0900

    name change

--- autc1.py
+++ autc1.py
@@ -24,8 +24,8 @@ class JobNode:
 	def __init__(self,whoami,input_keys,func,output_keys):
 		self._myname=whoami
 		# keys to check DB
-		self._input_keys=input_keys # keys = dictionary of  variable_name: list_of_link_id
-		self._output_keys=output_keys # keys = dictionary of  variable_name: list_of_link_id 
+		self._input_key_port=input_keys # keys = dictionary of  variable_name: list_of_link_id
+		self._output_key_portlist=output_keys # keys = dictionary of  variable_name: list_of_link_id 
 		# function to be called
 		self._func=func
 		# save values from DB
@@ -38,16 +38,16 @@ class JobNode:
 		    db=simpledb
 		    values={}
                     found=0
-		# assume that len of _output_keys is 1
-		    for key in self._output_keys:
-				outputid = self._output_keys[key]  # outputid is a list 
+		# assume that len of _output_key_portlist is 1
+		    for key in self._output_key_portlist:
+				outputid = self._output_key_portlist[key]  # outputid is a list 
 				if len(outputid)==0:
 					print "error in operation_OR"
 					print "outputid is null"
 					sys.exit(100000)
 	
-                    for key in self._input_keys:
-                        id_=self._input_keys[key]
+                    for key in self._input_key_port:
+                        id_=self._input_key_port[key]
                         if id_ in db:
 				values[id_]=db[id_]
 				found= 1
@@ -68,28 +68,28 @@ class JobNode:
 				found=self.operation_OR()
 		else:
 		    found=1
-		    for key in self._input_keys:
-			id_=self._input_keys[key]
+		    for key in self._input_key_port:
+			id_=self._input_key_port[key]
 			if id_ in db:
 				found=found and 1
 			else:
 				found=found and 0 
 		    if found:
-			print self._input_keys
-			print self._output_keys
+			print self._input_key_port
+			print self._output_key_portlist
 			if self._finished==0:
-				self._func(self._input_keys,self._output_keys)
+				self._func(self._input_key_port,self._output_key_portlist)
 				self._finished=1
 		return found 
 
 	def force_start(self):
 		if self._finished==0:
-                	self._func(self._input_keys,self._output_keys)
+                	self._func(self._input_key_port,self._output_key_portlist)
 			self._finished=1
 		return 1
 
 	def show(self):
-		print self._myname,self._input_keys,self._output_keys,self._input_values,self._output_values
+		print self._myname,self._input_key_port,self._output_key_portlist,self._input_values,self._output_values
 
 
 class JobNetwork:
@@ -103,15 +103,15 @@ class JobNetwork:
 			id_=hash_generator.get()  # is is string 
 		parent_node=parent[0]
 		parent_key=parent[1]
-		if parent_key in parent_node._output_keys:
-			parent_node._output_keys[parent_key].append( id_ )
+		if parent_key in parent_node._output_key_portlist:
+			parent_node._output_key_portlist[parent_key].append( id_ )
 		else:
 			print "failed to connect",parent ,"with", id
 			sys.exit(1000)
 		child_node=child[0]
 		child_key=child[1]
-		if child_key in child_node._input_keys:
-			child_node._input_keys[child_key] = id_
+		if child_key in child_node._input_key_port:
+			child_node._input_key_port[child_key] = id_
 		else:
 			print "failed to connect",child ,"with", id
 			sys.exit(1001)
@@ -130,25 +130,25 @@ class JobList():
 class RunnableNode:
 	""" accept input and output """
 	def __init__(self,args):
-		self._input_keys=args[0]
-		self._output_keys=args[1]
+		self._input_key_port=args[0]
+		self._output_key_portlist=args[1]
 
 		self._input_values={}
-		for key in self._input_keys:
-			id_=self._input_keys[key]
+		for key in self._input_key_port:
+			id_=self._input_key_port[key]
 			value=simpledb[id_]
 			self._input_values[key]=value
 
 		print "run"
-		print self._input_keys
+		print self._input_key_port
 		print self._input_values
 
-		print self._output_keys
+		print self._output_key_portlist
 
 	def writeoutput(self, outputvalues):
                 self._output_values={}
-                for key in self._output_keys:
-                        id_=self._output_keys[key]
+                for key in self._output_key_portlist:
+                        id_=self._output_key_portlist[key]
                         simpledb[id_]=output_values[key]
 
 def test3():
